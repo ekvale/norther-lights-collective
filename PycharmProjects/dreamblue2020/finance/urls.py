b605762ecffe23f11cfdaf7841d66eb39ef2e3be
Anon A Mouse <lionsmane@protonmail.com>
2024-07-04 16:15:33 -0500

    Clean up repository and remove unrelated files and directories

--- PycharmProjects/dreamblue2020/finance/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.property_list, name='list'),
-    path('<slug:property_slug>', views.property_detail, name='detail')
-]
--- PycharmProjects/dreamblue2020/finance/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def property_list(request):
-    return render(request, 'finance/property-list.html')
-
-def property_detail(request, property_slug):
-    # Fetching the correct property
-    return render(request, 'finance/property-detail.html')
